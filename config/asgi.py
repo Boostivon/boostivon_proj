82a04f62646879c8dc3f67ddbaf5cfcab7647f05
codesignature <codesignature19@gmail.com>
2026-05-24 17:09:13 -0400

    initial commit

--- config/asgi.py
+++ config/asgi.py
@@ -14,3 +14,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
 
 application = get_asgi_application()
+
+app = application  # for compatibility with some hosting providers
